f23c02ccb4215c9b4cb2c40a026d0bc4285186c9
郑仁杰 <zhengrenjie@bytedance.com>
2019-04-12 21:35:16 +0800

    chore: update eslint rules(delete line-break)

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
   rules: {
     "no-console": "off",
     indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
     quotes: ["error", "single"],
     semi: ["error", "never"],
     complexity: ["error", 4]
